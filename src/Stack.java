86aeca89d84ac4ce6c31b0d714296da8bc37cdc1
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-11-19 19:40:03 -0500

    Add comments for stack limit and variable naming
    Added comments suggesting improvements for stack limit and variable naming.

--- src/Stack.java
+++ src/Stack.java
@@ -1,6 +1,11 @@
 public class Stack {
 
+    // Hardcoding a limit of 10,000 might not be the best option
+    // May be better to have a constructor that lets you specify the max amount?
     private Space[] stack = new Space[10000];
+
+    // I'd be more clear with the naming here since "nStackPointer" doesn't necessarily mean much until you read push and pop
+    // Perhaps something like "currentStackIndex" or something similar that is shorter
     private int nStackPointer = 0;
 
     public void push(Space sp){
